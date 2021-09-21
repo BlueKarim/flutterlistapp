c7751eacd5949d97bab5c0f9dc67502594c7c548
unknown <khanhhoa3997@gmail.com>
2021-09-21 15:19:41 +0700

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(flutterlistapp());
 
 class flutterlistapp extends StatelessWidget {
+  const flutterlistapp({Key? key}):super(key: key);
   @override
   Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-          ),
-        body: const Center(
-            child: Text('Hello World'),
-          ),
-        ),
+      // title: 'Welcome to Flutter',
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: const Text('Welcome to Flutter'),
+      //     ),
+      //   body: Center(
+      //       child: RandomWords(),
+      //     ),
+      //   ),
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({ Key? key }) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 32.0, color: Colors.blueAccent);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context,i){
+        if(i.isOdd) 
+          return const Divider();
+
+          final index = i;
+          if(index >= _suggestions.length){
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
     );
   }
 }
